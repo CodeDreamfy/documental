013bc6fbd1b25f9d8ad0887f3693f1ce80248f43
codedreamimac <codedreamfy@outlook.com>
2017-12-28 18:40:09 +0800

    导航

--- src/components/core-header.vue
+++ src/components/core-header.vue
@@ -1,7 +1,18 @@
 <template>
-  <nav>
-    daohang
-  </nav>
+  <div class="mod-wrap nav">
+    <nav class="grid">
+      <a class="logo" href="javascript:;">
+        <img src="../assets/logo.png" alt="">
+      </a>
+      <div class="navlist">
+        <a href="javascript:;">首页</a>
+        <a href="javascript:;">技术笔记</a>
+        <a href="javascript:;">生活随笔</a>
+        <a href="javascript:;">音乐分享</a>
+        <a href="javascript:;">电影推荐</a>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
