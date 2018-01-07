c2b31835b90b798229f98ce87cd6ffddf8b419d8
codedream <codedreamfy@outlook.com>
2018-01-08 00:21:37 +0800

    add at-ui

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import AtComponents from 'at-ui';
+import 'at-ui-style/css/at.css';
 import App from './App';
 import router from './router';
 import './css/main.css';
 
 Vue.config.productionTip = false;
-
+Vue.use(AtComponents);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="core-box">
-    {{ msg }}
+    <div class="row at-row no-gutter flex-between">
+      <at-card class="at-card-item" :body-style="{ padding: 0 }">
+        <div>
+          <img style="width: 100%" src="https://misc.aotu.io/koppthe/at-ui/cover.jpg">
+          <div style="padding: 14px;">
+            <p>AT-UI 1</p>
+            <p>DESC desciption</p>
+          </div>
+        </div>
+      </at-card>
+      <at-card  class="at-card-item" :body-style="{ padding: 0 }">
+        <div>
+          <img style="width: 100%" src="https://misc.aotu.io/koppthe/at-ui/cover.jpg">
+          <div style="padding: 14px;">
+            <p>AT-UI 2</p>
+          </div>
+        </div>
+      </at-card>
+      <at-card  class="at-card-item" :body-style="{ padding: 0 }">
+        <div>
+          <img style="width: 100%" src="https://misc.aotu.io/koppthe/at-ui/cover.jpg">
+          <div style="padding: 14px;">
+            <p>AT-UI 3</p>
+          </div>
+        </div>
+      </at-card>
+      <at-card style="width: 300px;" :body-style="{ padding: 0 }">
+        <div>
+          <img style="width: 100%" src="https://misc.aotu.io/koppthe/at-ui/cover.jpg">
+          <div style="padding: 14px;">
+            <p>AT-UI</p>
+          </div>
+        </div>
+      </at-card>
+    </div>
   </div>
 </template>
 
@@ -14,6 +48,12 @@ export default {
     };
   },
   components: {
+  },
+  beforeCreate() {
+    this.$Loading.start();
+  },
+  mounted() {
+    this.$Loading.finish();
   },
 };
 </script>
